5e1c690abd4a2eba89a6b0bcb45c5cd38be343bf
DevidMonster <mrbat905@gmail.com>
2023-07-14 20:53:07 +0700

    config .env

--- src/app.js
+++ src/app.js
@@ -5,18 +5,21 @@ import categoryRouter from "./routers/categories"
 import authRouter from "./routers/auth"
 import cartRouter from "./routers/carts"
 import orderRouter from "./routers/orders"
+import dotenv from "dotenv";
 import cors from "cors"
 const app = express();
+dotenv.config()
 app.use(cors())
 app.use(express.json())
+
 
 app.use("/api",productRouter)
 app.use("/api",categoryRouter)
 app.use("/api",authRouter)
 app.use("/api",cartRouter)
 app.use("/api",orderRouter)
-
-mongoose.connect("mongodb+srv://thanhtvph28098:Thanh20033@react-service.ngzjrst.mongodb.net/?retryWrites=true&w=majority")
+console.log(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL)
 .then(()=>console.log("kết nối thành công React-N3"))
 .catch(err=>console.log(err))
 export const viteNodeApp = app
